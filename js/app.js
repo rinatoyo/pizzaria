8237c58adac187317ca8d5c1b2e4b663b8c7c6c8
Rina Toyoshiba <rtoyoshiba@gmail.com>
2019-01-25 18:20:16 -1000

    pizza

---
+++ js/app.js
@@ -0,0 +1,5 @@
+var newName = document.getElementById('name');
+newName.innerHTML = 'CATIZZA';
+
+var newTag = document.getElementById('tagLine');
+newTag.innerHTML = 'Pizza loved by everyone- Web developers and even cats!';
